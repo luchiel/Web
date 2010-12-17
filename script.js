2cffb6a6f931810c754954eb3b9d8f9f80f5ee8d
luchiel <luchiella@yandex.ru>
2010-12-18 02:36:08 +1000

    Some sorrowful happiness

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function() {
+	var format = "dd-MM-yy";
+	var date1 =
+		$("#date1").datepicker(
+			{
+				dateFormat: format
+			}
+		);
+	var date2 =
+		$("#date2").datepicker(
+			{
+				dateFormat: format
+			}
+		);
+});
