b3dec600a660f8f8a4cf62bcdca2ef3d0ba2b93c
luchiel <luchiella@yandex.ru>
2010-12-23 22:31:04 +1000

    Tired of this not-working Format

--- calendar.pl
+++ calendar.pl
@@ -1,29 +1,32 @@
-#!/usr/bin/perl
+#!/strawberry-perl/perl/bin/perl
 use strict;
 use warnings;
-use DateTime;
-use DateTime::Format::Strptime;
+use Class::Date;
+use Date::Parse;
+use Date::Format;
 use CGI;
+
+print "\n\n";
 
 my $q = CGI->new;
 
-my $format = new DateTime::Format::Strptime
-(
-	pattern => '%d-%B-%Y'
-	#,
-	#locale => 'en_AU'
-);
+my $d1 = Class::Date->new(str2time($q->param("date1")));
+if(!defined($d1))
+{
+	$d1 = Class::Date->new([2010, 01, 01]);
+}
 
+my $d2 = Class::Date->new(str2time($q->param("date2")));
+if(!defined$d2)
+{
+	$d2 = Class::Date->new([2010, 01, 02]);
+}
 
-my $d1 = $format->parse_datetime($q->param('date1'));
-my $d2 = $format->parse_datetime($q->param('date2'));
-$d1 = DateTime->new(year => 2000) if !defined($d1);
-$d2 = DateTime->new(year => 2000) if !defined($d2);
+my $days_between = (Class::Date->new($d2) - $d1)->day;
 
-my $d = 1;
-$d = $d2->delta_md($d1);
-#$dt->delta_md( $datetime )
-#$dt->delta_days( $datetime )
+my $format = "%d-%B-%Y";
+print time2str($format, $d1);
+#$d2 = time2str("%d-%B-%Y", $d2);
 
 print <<HERE
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -32,23 +35,24 @@ print <<HERE
 		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 		<script type="text/javascript" src="http://yandex.st/jquery/1.4.2/jquery.min.js"></script>
 		<script type="text/javascript" src="http://yandex.st/jquery-ui/1.8.2/jquery-ui.js"></script>
-		<script type="text/javascript" src="H:/client/script.js"></script>
+		<script type="text/javascript" src="script.js"></script>
 		<title>Calendar</title>
 	</head>
 	<body>
 		<form action="calendar.pl" method="GET">
 		<table>
 			<tr>
-				<td><input type="text" id="date1" name="date1" /></td>
-				<td><input type="text" id="date2" name="date2" /></td>
-				<td><input type="submit" value="Count interval!" name="submit" /></td>
+				<td id="output" colspan="3">
+					Difference: $days_between days
+				</td>
 			</tr>
-			<tr colspan="3">
-				<td id="output">$d</td>
+			<tr>
+				<td><input type="text" id="date1" name="date1" value="$d1"/></td>
+				<td><input type="text" id="date2" name="date2" value="$d2"/></td>
+				<td><input type="submit" value="Count interval!" name="submit" /></td>
 			</tr>
 		</table>
 		</form>
 	</body>
-<html>
+</html>
 HERE
-
